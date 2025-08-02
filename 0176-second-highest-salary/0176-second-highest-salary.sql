0c3fa7fc3115ae4157a209784dead64c2ed2cb78
Venkateshwarlu <23315a0528@cse.sreenidhi.edu.in>
2025-08-02 06:56:49 +0530

    Time: 292 ms (29.35%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,13 @@
 # Write your MySQL query statement below
-select max(salary) as SecondHighestSalary from Employee where salary<
-(select max(salary) from Employee);
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+
+
+/* 
+in above case  when more then one salary exist from give salary then code inside the brackets will excute 
+other wise rest code will eexcute result as null 
+*/
